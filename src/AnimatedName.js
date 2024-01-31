6d1a8b1d8450b99965e0ddf8ac41f43a8319fc6a
dellabeneta <m.dellabeneta@gmail.com>
2024-01-30 21:56:30 -0300

    teste reactjs

---
+++ src/AnimatedName.js
@@ -0,0 +1,23 @@
+// AnimatedName.js
+import React from 'react';
+import './AnimatedName.css';
+
+const AnimatedName = () => {
+  return (
+    <div className="animated-name">
+      <span>d</span>
+      <span>e</span>
+      <span>l</span>
+      <span>l</span>
+      <span>a</span>
+      <span>b</span>
+      <span>e</span>
+      <span>n</span>
+      <span>e</span>
+      <span>t</span>
+      <span>a</span>
+    </div>
+  );
+}
+
+export default AnimatedName;
